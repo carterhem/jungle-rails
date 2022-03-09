18def2dfaec7c1cc7a5f47299d9d8436e2147038
Carter Hemington <carter.hemington@gmail.com>
2022-03-09 20:34:16 +0000

    update admin dashboard controller show, add header to dashboard page

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,5 +1,9 @@
 class Admin::DashboardController < ApplicationController
   http_basic_authenticate_with name: ENV['USERNAME'], password: ENV['PASSWORD']
+  
   def show
+    def index
+      @product = Product.all
+    end
   end
 end
