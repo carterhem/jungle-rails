aad030cd14a0f9274f4e80d35e4b0738d9abfe6c
Carter Hemington <carter.hemington@gmail.com>
2022-03-10 19:23:03 +0000

    add sessions controller, define in routes.rb, new page confirmed working

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,5 +17,6 @@ class UsersController < ApplicationController
 
   def user_params
     params.require(:user).permit(:first_name, :last_name, :password, :password_confirmation)
+  end
   
 end
--- config/routes.rb
+++ config/routes.rb
@@ -20,8 +20,15 @@ Rails.application.routes.draw do
     resources :categories, only: [:index, :new, :create]
   end
 
+  #users routes
   get '/signup' => 'users#new'
   post '/users' => 'users#create'
+
+  #session routes
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
+
   
 
   # The priority is based upon order of creation: first created -> highest priority.
