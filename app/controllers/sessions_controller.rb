ce80a2f990c3508f6f345069da60cca3b04173a8
Carter Hemington <carter.hemington@gmail.com>
2022-03-10 20:21:17 +0000

    add Login/Signup to nav bar, add session logic to sessions controller, add current user to application controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,10 +4,26 @@ class SessionsController < ApplicationController
   end
 
   def create
+    user = User.find_by_email(params[:email])
+    #if user exists and password is correct
+    if user && user.authenticate(params[:password])
+      #save user id inside browser cookie
+      #this is how we keep user logged in when they navigate around our website
+      session[:user_id] = user.id
+      redirect_to '/'
+    else
+      #if users login doesn't work, send them back to login form
+      ##can replace with error message if desired
+      redirect_to '/login'
+    end
+
   end
 
   def destroy
+    session[:user_id] = nil
+    #could replace below with confirmation of error
+    redirect_to '/login'
   end
 
-  
+
 end
