5ed4038fbf9450881054d57ad26ab8464a3ea7ae
Carter Hemington <carter.hemington@gmail.com>
2022-03-14 19:31:01 +0000

    add product validation tests

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,51 +1,63 @@
 require 'rails_helper'
 
+
 RSpec.describe Product, type: :model do
   describe 'Validations' do
-    context 'given all 4 fields' do
+    context 'given all 4 validation fields' do
       it 'should save successfully' do
-      #set all fields to a value but validation field - NA here
-      #test that expect error is found within the .errors.full_messages array (include?)
+        @category = Category.new
+       @product = Product.new(name: "First", price: 10, quantity: 2, category: @category)
+        expect(@product.name).to be_present
+        expect(@product.price).to be_present
+        expect(@product.quantity).to be_present
+        expect(@product.category).to be_present
+        @product.save 
+        expect(@product.errors.full_messages).to eql([])
+      end
     end
     
     context 'if the name field is missing' do
       it 'should not save' do
-        #set all fields to a value but validation field - NA here
-      #test that expect error is found within the .errors.full_messages array (include?)
-
+      @category = Category.new
+      @product = Product.new(price: 10, quantity: 2, category: @category)
+      @product.save
+      expect(@product.errors.full_messages).to include("Name can't be blank")
       end
 
     end
     
+   
     context 'if the price field is missing' do
       it 'should not save' do
-        #set all fields to a value but validation field - NA here
-        #test that expect error is found within the .errors.full_messages array (include?)
-
+      @category = Category.new
+      @product = Product.new(name: "first", quantity: 2, category: @category)
+      @product.save
+      expect(@product.errors.full_messages).to include("Price cents is not a number", "Price is not a number", "Price can't be blank")
       end
 
     end
+
 
     context 'if the quantity field is missing' do
       it 'should not save' do
-        #set all fields to a value but validation field - NA here
-      #test that expect error is found within the .errors.full_messages array (include?)
-
+      @category = Category.new
+      @product = Product.new(name: "first", price: 10, category: @category)
+      @product.save
+      expect(@product.errors.full_messages).to include("Quantity can't be blank")
       end
 
     end
+
 
     context 'if the category field is missing' do
       it 'should not save' do
-        #set all fields to a value but validation field - NA here
-      #test that expect error is found within the .errors.full_messages array (include?)
-
+      @category = Category.new
+      @product = Product.new(name: "first", price: 10, quantity: 2)
+      @product.save
+      expect(@product.errors.full_messages).to include("Category can't be blank")
       end
 
     end
-
-
-
   end
 
 
