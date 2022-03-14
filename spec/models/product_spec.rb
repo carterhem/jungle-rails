18068a7a58799e6bc5e7ee6f4b8f58d3b691a498
Carter Hemington <carter.hemington@gmail.com>
2022-03-14 17:16:47 +0000

    shell created for product tests

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,5 +1,53 @@
 require 'rails_helper'
 
 RSpec.describe Product, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'Validations' do
+    context 'given all 4 fields' do
+      it 'should save successfully' do
+      #set all fields to a value but validation field - NA here
+      #test that expect error is found within the .errors.full_messages array (include?)
+    end
+    
+    context 'if the name field is missing' do
+      it 'should not save' do
+        #set all fields to a value but validation field - NA here
+      #test that expect error is found within the .errors.full_messages array (include?)
+
+      end
+
+    end
+    
+    context 'if the price field is missing' do
+      it 'should not save' do
+        #set all fields to a value but validation field - NA here
+        #test that expect error is found within the .errors.full_messages array (include?)
+
+      end
+
+    end
+
+    context 'if the quantity field is missing' do
+      it 'should not save' do
+        #set all fields to a value but validation field - NA here
+      #test that expect error is found within the .errors.full_messages array (include?)
+
+      end
+
+    end
+
+    context 'if the category field is missing' do
+      it 'should not save' do
+        #set all fields to a value but validation field - NA here
+      #test that expect error is found within the .errors.full_messages array (include?)
+
+      end
+
+    end
+
+
+
+  end
+
+
+
 end
